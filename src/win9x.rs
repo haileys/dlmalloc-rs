0e90b1d2ccc3c993f0e69d693d26a4a1dcbe78ff
Hailey Somerville <hailey@hailey.lol>
2023-05-15 23:30:47 +1000

    remove manual critical section in favour of win9x-sync crate

--- src/win9x.rs
+++ src/win9x.rs
@@ -3,20 +3,10 @@ use core::convert::TryFrom;
 use core::ffi::c_void;
 use Allocator;
 
+use win9x_sync::critical_section::CriticalSection;
+
 mod winapi {
     use core::ffi::{c_void, c_ulong, c_long};
-
-    #[allow(non_camel_case_types)]
-    #[allow(non_snake_case)]
-    #[repr(C)]
-    pub struct CRITICAL_SECTION {
-        DebugInfo: *mut c_void,
-        LockCount: c_long,
-        RecursionCount: c_long,
-        OwningThread: c_ulong,
-        LockSemaphore: c_ulong,
-        SpinCount: c_ulong,
-    }
 
     pub const MEM_COMMIT: c_ulong = 0x00001000;
     pub const MEM_RESERVE: c_ulong = 0x00002000;
@@ -38,12 +28,6 @@ mod winapi {
             dwSize: c_ulong,
             dwFreeType: c_ulong,
         ) -> bool;
-
-        pub fn InitializeCriticalSection(ptr: *mut CRITICAL_SECTION);
-        pub fn EnterCriticalSection(ptr: *mut CRITICAL_SECTION);
-        pub fn LeaveCriticalSection(ptr: *mut CRITICAL_SECTION);
-
-        pub fn Sleep(dwMilliseconds: c_ulong);
     }
 }
 
@@ -57,9 +41,6 @@ impl System {
         System { _priv: () }
     }
 }
-
-// #[cfg(feature = "global")]
-// static mut LOCK: libc::pthread_mutex_t = libc::PTHREAD_MUTEX_INITIALIZER;
 
 unsafe impl Allocator for System {
     fn alloc(&self, size: usize) -> (*mut u8, usize, u32) {
@@ -127,58 +108,16 @@ unsafe impl Allocator for System {
     }
 }
 
-mod critical_section {
-    use core::mem::MaybeUninit;
-    use core::sync::atomic::{AtomicU32, Ordering};
-    use super::winapi;
-
-    static mut LOCK: MaybeUninit<winapi::CRITICAL_SECTION> = MaybeUninit::uninit();
-
-    // win9x never supported SMP, but ideally this works
-    // on newer versions of windows too
-    static LOCK_INIT: AtomicU32 = AtomicU32::new(0);
-
-    pub fn get() -> *mut winapi::CRITICAL_SECTION {
-        while LOCK_INIT.load(Ordering::SeqCst) != 2 {
-            let init_val = LOCK_INIT.compare_exchange(
-                0,
-                1,
-                Ordering::SeqCst,
-                Ordering::SeqCst,
-            );
-
-            match init_val {
-                Ok(_) => {
-                    // we got it!
-                    unsafe {
-                        winapi::InitializeCriticalSection(LOCK.as_mut_ptr());
-                    }
-
-                    LOCK_INIT.store(2, Ordering::SeqCst);
-                }
-                Err(_) => {
-                    // another thread got it.
-                    // sleeping for 1 millisecond is the closest thing to a
-                    // yield we have on this platform
-                    unsafe { winapi::Sleep(1); }
-                }
-            }
-        }
-
-        unsafe { LOCK.as_mut_ptr() }
-    }
-}
+static LOCK: CriticalSection = CriticalSection::new();
 
 #[cfg(feature = "global")]
 pub fn acquire_global_lock() {
-    let crit = critical_section::get();
-    unsafe { winapi::EnterCriticalSection(crit); }
+    unsafe { LOCK.enter(); }
 }
 
 #[cfg(feature = "global")]
 pub fn release_global_lock() {
-    let crit = critical_section::get();
-    unsafe { winapi::LeaveCriticalSection(crit); }
+    unsafe { LOCK.leave(); }
 }
 
 #[cfg(feature = "global")]
